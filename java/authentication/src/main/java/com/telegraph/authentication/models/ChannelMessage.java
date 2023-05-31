fed0165380538475c9fc2731eea37643df992feb
alextereshyt <alextereshyt@gmail.com>
2023-05-31 15:24:36 +0300

    Added base api and swagger

---
+++ java/authentication/src/main/java/com/telegraph/authentication/models/ChannelMessage.java
@@ -0,0 +1,29 @@
+package com.telegraph.authentication.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "channelMessages")
+public class ChannelMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "senderUserId", nullable = false)
+    private User senderUserChannelMessage;
+
+    @ManyToOne
+    @JoinColumn(name = "channelId", nullable = false)
+    private Channel receiverChannelMessage;
+
+    private String message;
+}
