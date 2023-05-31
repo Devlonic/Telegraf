46abfbeb3701e841f4e6956ff02ac981096b14d2
Devloner <tminecraftben@gmail.com>
2023-05-31 18:52:09 +0300

    added dependencies bs/axios

---
+++ react/telegraph-app/src/components/containers/default/DefaultLayout.tsx
@@ -0,0 +1,25 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import DefaultHeader from "./DefaultHeader";
+import { useEffect } from "react";
+// import { isSignedIn } from "../../../services/tokenService";
+
+const DefaultLayout = () => {
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    console.log("DefaultLayout useEffect");
+    // if (isSignedIn() == false) {
+    //   navigator("/auth/login");
+    // }
+  }, []);
+
+  return (
+    <>
+      <DefaultHeader />
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
+  );
+};
+export default DefaultLayout;
