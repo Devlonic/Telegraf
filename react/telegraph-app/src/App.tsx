60920b4b7bacef351ff80d6e0937a6cdd3638da2
Devloner <tminecraftben@gmail.com>
2023-06-05 19:50:32 +0300

    added routes and auth layout/header

--- react/telegraph-app/src/App.tsx
+++ react/telegraph-app/src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./components/containers/default/DefaultLayout";
+import AuthLayout from "./components/containers/auth/AuthLayout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}></Route>
+      <Route path="/auth" element={<AuthLayout />}></Route>
+      <Route path="/chats" element={<DefaultLayout />}></Route>
+    </Routes>
   );
 }
 
